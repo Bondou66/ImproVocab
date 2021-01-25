3617fe7248db10246cb648a94aae6def6a83355f
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-01-25 09:24:07 -0800

    added Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=80)
+    
