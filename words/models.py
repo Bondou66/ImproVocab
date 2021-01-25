0f1e034c2e2b110955d878d3084ad34926f00493
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-01-25 10:09:03 -0800

    added string method to Word model

--- words/models.py
+++ words/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class Words(models.Model):
+class Word(models.Model):
     word = models.CharField(max_length=30)
     definition = models.TextField
     origins = models.CharField(max_length=30)
     context = models.TextField
     word_type = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.word
