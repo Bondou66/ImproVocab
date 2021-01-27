16ae3f1517fb80af235013f0179e27bcfbf5b135
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-01-27 11:02:47 -0800

    added word detail page

--- words/urls.py
+++ words/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
+app_name = "words"
+
 urlpatterns = [
-    path('wordlist/', views.word_list),
+    path("wordlist/", views.word_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.word_detail, name="detail"),
 ]
