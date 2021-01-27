b40ef97fd577ec6f481caddbab8547ff5c0a84fe
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-01-27 09:23:10 -0800

    added various webpages and data migrations

--- words/models.py
+++ words/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 class Word(models.Model):
     word = models.CharField(max_length=30)
-    definition = models.CharField(max_length=100)
+    definition = models.TextField()
     origins = models.CharField(max_length=30)
-    context = models.TextField
+    context = models.TextField()
     word_type = models.CharField(max_length=10)
-    slug = models.SlugField(max_length=100, default=word)
+    slug = models.SlugField(max_length=100)
 
     def __str__(self):
         return self.word
+
+    def snippet(self):
+        if len(self.definition) > 50:
+            return self.definition[:50] + "..."
+        else:
+            return self.definition
