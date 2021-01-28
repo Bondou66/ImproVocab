28cf54051ec2c96c1a7d3921b28a102806531619
Abanoub Farag <AbanoubMarcos.Farag@calbaptist.edu>
2021-01-28 12:10:06 -0800

    Added learning random words

--- words/views.py
+++ words/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Word, Learned
 from django.contrib.auth.decorators import login_required
 from . import forms
+import random
 
 def word_list(request):
     words = Word.objects.all().order_by('word_type', 'word')
@@ -12,14 +13,21 @@ def word_detail(request, slug):
     return render(request, "words/word_detail.html", {'word' : word})
 
 @login_required(login_url="/accounts/login/")
-def learn_word(request):
+def learn_word(request, slug=None):
     user = request.user
+    if slug != None:
+        word = Word.objects.get(slug=request.POST.get('slug'))
+    else:
+        learned_words = Learned.objects.values_list('word', flat=True).filter(user=user)
+        words = Word.objects.exclude(id__in=learned_words)
+        word = random.choice(words)
     learned = Learned()
-    word = Word.objects.get(slug=request.POST.get('slug'))
     learned.user = user
     learned.word = word
-    learned.save()
-    return render(request, "words/learn_word.html")
+    is_learned = Learned.objects.filter(user=user, word=word)
+    if learned not in is_learned:
+        learned.save()
+    return render(request, "words/learn_word.html", {'word' : word})
 
 @login_required(login_url="/accounts/login/")
 def practice_word(request):
